502072bdaa37a4565a151b4d3a0d2759fcf7e377
kys3189 <kimys31892@gmail.com>
2025-05-05 23:01:12 +0900

    header middle commit

--- src/pages/layout/header/Header.jsx
+++ src/pages/layout/header/Header.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { headerContainer, headerLeftContainer, headerLinkContainer, headerMainContainer, headerMainIconContainer, headerProfileContainer, headerRightContainer, headerSocialContainer } from './style';
+import alertIcon from './img/alert.png';
+import messageIcon from './img/message.png';
+import persenalBuddyIcon from './img/persenalBuddyIcon.png';
+import memberProfile from './img/memberProfile.png';
 
 
 const Header = () => {
+
     return (
-        <div>
-            <NavLink to={"/main"}>일정관리</NavLink>
-            <NavLink to={"/main/contents"}>컨텐츠</NavLink>
-            <NavLink to={"/main/community/event"}>이벤트</NavLink>
-            <NavLink to={"/main/community/board"}>커뮤니티</NavLink>
-            <NavLink to={"/main/faq"}>고객센터</NavLink>
+        <div style={headerContainer}>
+            <div style={headerMainContainer}>
+                <div style={headerLeftContainer}>
+                    <div style={headerMainIconContainer}>
+                        <img src={persenalBuddyIcon} alt="퍼스널 버디 아이콘" />
+                    </div>
+                    <div style={headerLinkContainer}>
+                        <NavLink to={"/main"}>일정관리</NavLink>
+                        <NavLink to={"/main/contents"}>컨텐츠</NavLink>
+                        <NavLink to={"/main/community/event"}>이벤트</NavLink>
+                        <NavLink to={"/main/community/board"}>커뮤니티</NavLink>
+                        <NavLink to={"/main/faq"}>고객센터</NavLink>
+                    </div>
+                </div>
+                <div style={headerRightContainer}>
+                    <div style={headerSocialContainer}>
+                        <img src={messageIcon} alt="메세지 아이콘" />
+                        <img src={alertIcon} alt="알림 아이콘" />
+                    </div>
+                    <div style={headerProfileContainer}>
+                        <img src={memberProfile} alt="멤버 프로필" />
+                        <span>nickName 님</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/pages/layout/header/style.js
@@ -0,0 +1,56 @@
+import theme from "../../../globals/theme";
+
+export const headerContainer = {
+    backgroundColor: theme.PALLETE.background.white,
+    display: "flex",
+    justifyContent: "center",
+    width: "100%",
+    height: "90px",
+    fontSize: theme.FONT_SIZE.h6,
+    fontColor: theme.PALLETE.black,
+    fontWeight: theme.FONT_WEIGHT.regular,
+};
+
+export const headerMainContainer = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "1400px",
+};
+
+export const headerLeftContainer = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+};
+
+export const headerRightContainer = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+};
+
+export const headerMainIconContainer = {
+    display: "flex",
+    alignItems: "center",
+    width: "70px",
+    height: "46px",
+}
+export const headerLinkContainer = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginLeft: "54px",
+    width: "558px",
+}
+export const headerSocialContainer = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "80px",
+}
+export const headerProfileContainer = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+}
