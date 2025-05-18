5722e5d24e2dccce287dc962bfd7b30054d3840e
sjunghoon10 <junghoon.shin3377@gmail.com>
2025-05-19 08:59:22 +0900

    router 추가

--- src/routes/router.js
+++ src/routes/router.js
@@ -62,7 +62,10 @@ import JoinAgree from "../pages/member/join/JoinAgree";
 import JoinContainer from "../pages/member/join/JoinContainer";
 import JoinInfo from "../pages/member/join/JoinInfo";
 import JoinProfile from "../pages/member/join/JoinProfile";
-
+import CalendarTodo from "../pages/main/calendar/calendarTodo/CalendarTodo";
+import CalendarSave from "../pages/main/calendar/calendarSave/CalendarSave";
+import ScheduleSave from "../pages/main/calendar/scheduleSave/ScheduleSave";
+import ScheduleView from "../pages/main/calendar/scheduleView/ScheduleView";
 import PrivacyPolicy from "../pages/privacy/PrivacyPolicy";
 import BoardPost from "../pages/community/board/post/BoardPost";
 
