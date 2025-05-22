18e6153a2028c9187197a2dbb2208b3366dd60fa
jiihyun12 <j22h.h0h@gmail.com>
2025-05-22 15:39:44 +0900

    banner 작업중

--- src/pages/layout/banner/Banner.jsx
+++ src/pages/layout/banner/Banner.jsx
@@ -1,29 +1,71 @@
-
-
-import React from 'react';
+import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/autoplay';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import S from './style'; // styled-components
-
+import S from './style';
+import { Autoplay } from 'swiper/modules';
 
 const Banner = () => {
+  const [swiper, setSwiper] = useState('');
+  const totalSlides = 3;
+  const [activeIndex, setActiveIndex] = useState(0);
+
+
+  const handleDotClick = (index) => {
+    if (!swiper) return;
+    swiper.slideToLoop(index);
+    setActiveIndex(index);
+    swiper.autoplay?.start();
+  };
+
   return (
     <S.BannerWrapper>
       <Swiper
-        spaceBetween={0}
+        modules={[Autoplay]}
         slidesPerView={1}
         loop={true}
-        autoplay={{ delay: 4000 }}
-        pagination={{ clickable: true }}
-        navigation={true}
+        autoplay={{ delay: 5000, disableOnInteraction: false }}
+        onSwiper={(swiper) => setSwiper(swiper)}
+        allowTouchMove={true}
+        navigation={false}
+        pagination={false}
       >
-        <SwiperSlide><img src="/assets/images/banner/banner-blue.png" alt="배너1" /></SwiperSlide>
-        <SwiperSlide><img src="/assets/images/banner/banner-purple.png" alt="배너2" /></SwiperSlide>
-        <SwiperSlide><img src="/assets/images/banner/banner-green.png" alt="배너3" /></SwiperSlide>
+        <SwiperSlide>
+          <img src="/assets/images/banner/banner-blue.png" alt="배너1" />
+          <S.Banner1Text>
+            <S.bannerTitle>나의 업적</S.bannerTitle>
+            <div>버디들의 업적을 <br/> 확인해보세요!</div>
+          </S.Banner1Text>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img src="/assets/images/banner/banner-purple.png" alt="배너2" />
+           <S.Banner2Text>
+            <S.bannerTitle>성장 나무</S.bannerTitle>
+            <div>버디들의 성장나무는 <br/> 확인해보세요!</div>
+          </S.Banner2Text>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img src="/assets/images/banner/banner-green.png" alt="배너3" />
+          <S.Banner2Text>
+            <S.bannerTitle>나의 업적</S.bannerTitle>
+            <div>버디들의 업적을 <br/> 확인해보세요!</div>
+          </S.Banner2Text>
+        </SwiperSlide>
       </Swiper>
+         <S.DotsContainer>
+        {[...Array(totalSlides)].map((_, i) => (
+          <S.Dot
+            key={i}
+            active={activeIndex === i}
+            onClick={() => handleDotClick(i)}
+          />
+        ))}
+      </S.DotsContainer>
+
     </S.BannerWrapper>
   );
 };
--- src/pages/layout/banner/style.js
+++ src/pages/layout/banner/style.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
+import { fontWeightRegular } from '../../../globals/common';
 
 const S = {};
 
 S.BannerWrapper = styled.div`
+  position: relative;
   width: 100%;
   max-width: 1920px;
   height: 300px;
@@ -21,10 +23,66 @@ S.BannerWrapper = styled.div`
   }
 
   .swiper-pagination-bullet-active {
-    background: #00C896;
+    background: #00C896 ;
   }
 `;
 
+S.SliderImage = styled.div`
+  width: 1920px;
+  height: 300px;
+`
+
+S.bannerTitle = styled.div`
+    font-size: 17px;
+    ${fontWeightRegular}
+    margin-bottom: 10px;
+`
+
+S.Dot = styled.span`
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background: ${({ active }) => (active ? '#00C896' : '#ddd')};
+    cursor: pointer;
+    transition: background 0.3s;
+`;
+
+S.DotsContainer = styled.div`
+    position: absolute; 
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    gap: 8px;
+    z-index: 10;
+`;
 
 
+S.Banner1Text = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 10%;
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+S.Banner2Text = styled.div`
+    position: absolute;
+  top: 50%;
+  left: 10%;
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+
+`
+S.Banner3Text = styled.div`
+    position: absolute;
+  top: 50%;
+  left: 10%;
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+
+`
 export default S;
